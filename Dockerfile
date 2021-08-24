91a6840dec3ada8d3f076f7c0296981a25bf0683
Marina Lobina <mvlobina@gmsil.com>
2021-08-25 00:33:48 +0300

    Configured postgres db

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,14 @@ MAINTAINER Marina Lobina
 
 ENV PYTHONUNBUFFERED 1
 
-RUN apk update && apk add postgresql-dev gcc python3-dev musl-dev
+
 COPY ./requirements.txt /requirements.txt
-RUN pip install -r /requirements.txt --no-cache-dir
+RUN apk add --update --no-cache postgresql-client
+RUN apk add --update --no-cache --virtual .tmp-build-deps \
+    gcc libc-dev linux-headers postgresql-dev
+
+RUN pip install -r /requirements.txt
+RUN apk del .tmp-build-deps
 
 RUN mkdir /app
 WORKDIR /app
--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,11 +78,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'recipe_app',
-        'USER': 'postgres',
-        'PASSWORD': 'mypassword',
-        'HOST': '10.0.65.2',
-        'PORT': '5432',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
+        "PORT": '5432'
+
     }
 }
 
